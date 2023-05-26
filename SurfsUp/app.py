02293dddcb992b8a0ba5d3261de8b879375f9d7c
Nicholas Simpson <114123854+Nicholas-Simpson@users.noreply.github.com>
2023-05-25 18:35:26 -0700

    finished part 2 hw
    finished up commenting all my code for the api

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,7 +1,5 @@
 # Import the dependencies.
 import numpy as np
-
-import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
@@ -13,7 +11,7 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///../Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///../Resources/hawaii.sqlite",echo=False)
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -41,46 +39,105 @@ app = Flask(__name__)
 def welcome():
     """List all available api routes."""
     return (
-        f"Available Routes:"
-        f"/api/v1.0/precipitation"
-        f"/api/v1.0/stations"
-        f"/api/v1.0/tobs"
-        f"/api/v1.0/<start>"
-        f"/api/v1.0/<start>/<end>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end<br/>"
     )
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    # Create session link
+    # Create our session (link) from Python to the DB
     session = Session(engine)
-
-    # Query precipitation 
+    # Set required variables needed for the session query
     year_ago = dt.date(2017,8,23) - dt.timedelta(days=365)
-    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= str(year_ago)).all()
-    all_prec_dates = []
-    for date, inches in results:
-        date_dict = {}
-        date_dict['Date'] = date
-        date_dict['prcp'] = inches
-        all_prec_dates.append(date_dict)
 
+    # query session using filter information to obtain results
+    results = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= str(year_ago)).all()
+    
+    # close Session link
+    session.close()
+
+    # Take results and turn into a json format for web use
+    all_prec_dates = {date:prcp for date, prcp in results}
     return jsonify(all_prec_dates)
+
 
 @app.route("/api/v1.0/stations")
 def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # query session using filter information to obtain results
+    results = session.query(Station.station).all()
+    all_stations = list(np.ravel(results))
+
+    # close Session link
+    session.close()
+
+    # Take results and turn into a json format for web use
+    return jsonify(all_stations)
 
 
 @app.route("/api/v1.0/tobs")
 def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Set required variables needed for the session query
+    active_station = 'USC00519281'
+    year_ago = dt.date(2017,8,23) - dt.timedelta(days=365)
+
+    # query session using filter information to obtain results
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == active_station).\
+            filter(Measurement.date >= year_ago).all()
+    active_station = list(np.ravel(results))
+
+    # close Session link
+    session.close()
+
+    # Take results and turn into a json format for web use
+    return jsonify(active_station)
 
 
 @app.route("/api/v1.0/<start>")
-def start_date():
-
-
 @app.route("/api/v1.0/<start>/<end>")
-def end_date():
+def stats(start = None, end = None):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Set required variables needed for the session query
+    select = [func.min(Measurement.tobs),func.max(Measurement.tobs),
+              func.avg(Measurement.tobs)]
     
+    # Set up an if statement to account for the route user input when end date is not present
+    if not end:
+        # Set up the string input into an actual date/time format
+        start = dt.datetime.strptime(start,'%Y-%m-%d')
+        # query session using filter information to obtain results
+        results = session.query(*select).filter(Measurement.date >= start).all()
+        # close Session link
+        session.close()
+        # Take results and turn into a json format for web use
+        query_results = list(np.ravel(results))
+        return jsonify(query_results)
+    
+    # Set up the string input into an actual date/time format
+    start = dt.datetime.strptime(start,'%Y-%m-%d')
+    end = dt.datetime.strptime(end,'%Y-%m-%d')
+    # query session using filter information to obtain results
+    results = session.query(*select).filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    # close Session link
+    session.close()
+    # Take results and turn into a json format for web use
+    query_results = list(np.ravel(results))
+    return jsonify(query_results)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
